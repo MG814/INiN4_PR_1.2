50625cb7170e6125c0390bdaa0037d392b622fa2
MICHAŁ <mg814@wp.pl>
2021-06-25 18:03:46 +0200

    Zadanie.1

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,45 @@
 package com.company;
 
 public class Animal {
+    public final String species;
+    private Double weight;
+    private final Double DEFAULT_WEIGHT = 1.0;
+
+    public Animal(String species){
+        this.species = species;
+        switch (species){
+            case "cow":
+                this.weight = 500.0;
+                break;
+            case "dog":
+                this.weight = 40.0;
+                break;
+            case "cat":
+                this.weight = 5.0;
+                break;
+            default:
+               this.weight = DEFAULT_WEIGHT;
+               break;
+        }
+    }
+
+    public void feed(){
+        if(weight > 0.0){
+            weight+=1;
+            System.out.println(species + " " + weight + " kg");
+        }
+        else
+            System.out.println("Zwierzę nie żyje.");
+    }
+
+    public void takeForAWalk(){
+        if(weight > 0.0){
+            weight-=1;
+            System.out.println(species + " " + weight + " kg");
+        }
+        else
+            System.out.println("Zwierzę nie żyje.");
+    }
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Animal animal1 = new Animal("dog");
+
+        animal1.feed();
+        animal1.takeForAWalk();
+        animal1.feed();
     }
 }
